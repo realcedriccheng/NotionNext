9fb8f117a6300f93cb6e93b1de3946a2f490961b
cedric <chengwj@hust.edu.cn>
2026-01-24 15:33:58 +0800

    featnew

--- conf/font.config.js
+++ conf/font.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // START ************网站字体*****************
   // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif',
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
@@ -22,6 +22,7 @@ module.exports = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
+    '"Noto Sans SC"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -30,7 +31,6 @@ module.exports = {
     '"Segoe UI Emoji"',
     '"Segoe UI Symbol"',
     '"Segoe UI"',
-    '"Noto Sans SC"',
     'HarmonyOS_Regular',
     '"Helvetica Neue"',
     'Helvetica',
@@ -42,8 +42,8 @@ module.exports = {
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
     // '"LXGW WenKai"',
-    'Bitter',
     '"Noto Serif SC"',
+    'Bitter',
     'SimSun',
     '"Times New Roman"',
     'Times',
--- themes/endspace/config.js
+++ themes/endspace/config.js
@@ -46,7 +46,7 @@ const CONFIG = {
   ENDSPACE_LOADING_TEXT_FADEOUT: 'ENTERING', // 淡出阶段显示的文字
   
   // 加载动画中的图片，可以使用public目录下的图片如'/images/logo.png'，也可以使用url如https://example.com/image.png
-  ENDSPACE_LOADING_IMAGE: '/svg/loader-circle-64px.svg', // 加载动画中显示的图片
+  ENDSPACE_LOADING_IMAGE: '/images/orange_15164267.png', // 加载动画中显示的图片
 
   // ============================================
   // 水印文字配置 (Watermark Text)
